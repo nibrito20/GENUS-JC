7ef283bcaec5fa54207c764f4f83521d1c2d9f25
nibrito20 <minorbrito20@gmail.com>
2025-12-06 13:23:02 -0300

    Corrigido backend API + rotas

--- Back-end/project/urls.py
+++ Back-end/project/urls.py
@@ -7,11 +7,10 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('webhook/deploy/', deploy_views.deploy_webhook, name='deploy_webhook'),
-    path('jornal/', include('jornal.urls')), 
+    path('', include('jornal.urls')), 
     path('avaliacoes/', include('aval.urls')),
-    path('', views.index, name='index'), 
+    # path('', views.index, name='index'), 
 ]
 
 if settings.DEBUG:
