af28c79c12fdc91acc4ad63a884c1ad67032ec41
nibrito20 <minorbrito20@gmail.com>
2025-12-06 10:36:17 -0300

    reconfigurando a rota do STATICFILES_DIRS

--- Back-end/project/settings.py
+++ Back-end/project/settings.py
@@ -66,7 +66,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'front-end/dist'),
+    os.path.join(BASE_DIR, '../front-end/dist'),
 ]
 
 # Templates desabilitados - usando React no front-end
