2a0b4398c12c1103d571baa994344afb49fe27dc
viniinitc <vinitcavalcantena@gmail.com>
2025-12-06 22:10:32 -0300

    teste2

--- Back-end/project/settings.py
+++ Back-end/project/settings.py
@@ -66,7 +66,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    '/home/genuss/GENUS-JC/front-end/dist',
+   '/home/genuss/GENUS-JC/front-end/dist',
 ]
 
 # Templates desabilitados - usando React no front-end
