d489ed4a67c9b73eebc695074f825d76bd536fd5
nibrito20 <minorbrito20@gmail.com>
2025-12-08 21:12:32 -0300

    Ajustando teste e2e barra de pesquisa

--- front-end/cypress/e2e/pesquisa_noticia.cy.ts
+++ front-end/cypress/e2e/pesquisa_noticia.cy.ts
@@ -1,5 +1,3 @@
-/// <reference types="cypress" />
-
 describe("Barra de Pesquisa", () => {
   beforeEach(() => {
     cy.intercept("GET", "http://localhost:8000/api/noticias/*").as("getNoticias");
@@ -9,20 +7,15 @@ describe("Barra de Pesquisa", () => {
   });
 
   it("deve permitir pesquisar uma noticia", () => {
-    // abrir barra de busca
     cy.get('img[alt="Buscar"]').first().click();
 
-    // digitar termo e enviar
     cy.get('input[placeholder="Pesquisar"]').type("economia{enter}");
 
-    // verificar redirecionamento
     cy.url().should("contain", "/noticias?q=economia");
 
-    // intercept da pesquisa
     cy.intercept("GET", /api\/noticias\/\?q=economia/).as("pesquisaNoticias");
     cy.wait("@pesquisaNoticias");
 
-    // deve exibir pelo menos 1 card
     cy.get(".news-container")
       .should("exist")
       .and("be.visible")
@@ -41,7 +34,6 @@ describe("Barra de Pesquisa", () => {
     cy.intercept("GET", new RegExp(`/api/noticias/\\?q=${termo}`)).as("pesquisaReq");
     cy.wait("@pesquisaReq");
 
-    // não usar "be.visible" porque a div pode ficar com altura zero
     cy.get(".news-container")
       .should("exist")
       .within(() => {
