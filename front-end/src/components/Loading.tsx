6597effd6aa1432077131b3faa46fab91ff681c9
jotadiasss <joaopefdias@gmail.com>
2025-12-10 16:49:41 -0300

    tudo q tem pra commitar 2

--- front-end/src/components/Loading.tsx
+++ front-end/src/components/Loading.tsx
@@ -18,7 +18,7 @@ export default Loading
 const LoadingNews = () => {
     return (
       <div className="loading-style">
-        <h1>Carregando noticias</h1>
+        <h1>Carregando notícias</h1>
         <div className="loading-points-style">
           <div></div>
           <div></div>
