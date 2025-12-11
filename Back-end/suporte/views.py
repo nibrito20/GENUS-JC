bc9cc307fdd32f1160a06d9bc24abd7b66d53f0f
Joao Dias <167938973+JotaDiasss@users.noreply.github.com>
2025-12-11 00:34:26 -0300

    arrumando suporte novamente
    arrumando suporte

--- Back-end/suporte/views.py
+++ Back-end/suporte/views.py
@@ -1,11 +1,13 @@
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from .models import SupportTicket
 from .serializers import SupportTicketSerializer
+from jornal.views import SemCSRF
 
 @api_view(['POST'])
+@authentication_classes([SemCSRF])
 @permission_classes([IsAuthenticated])
 def submit_support_ticket(request):
     """
